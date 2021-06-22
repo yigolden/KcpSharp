030915109af9a384a818ebee65b7303fe4e2cbbe
yigolden <jinyi@live.cn>
2021-06-22 20:44:34 +0800

    Allow setting exception handler on socket transport.

--- src/KcpSharp/KcpSocketTransportForConversation.cs
+++ src/KcpSharp/KcpSocketTransportForConversation.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
 
 namespace KcpSharp
@@ -11,6 +12,10 @@ namespace KcpSharp
         private readonly int _conversationId;
         private readonly KcpConversationOptions? _options;
 
+        private Func<Exception, IKcpTransport<KcpConversation>, object?, bool>? _exceptionHandler;
+        private object? _exceptionHandlerState;
+
+
         internal KcpSocketTransportForConversation(Socket socket, EndPoint endPoint, int conversationId, KcpConversationOptions? options)
             : base(socket, endPoint, options?.Mtu ?? KcpConversationOptions.MtuDefaultValue)
         {
@@ -20,5 +25,22 @@ namespace KcpSharp
 
         /// <inheritdoc />
         protected override KcpConversation Activate() => new KcpConversation(this, _conversationId, _options);
+
+        protected override bool HandleException(Exception ex)
+        {
+            if (_exceptionHandler is not null)
+            {
+                return _exceptionHandler.Invoke(ex, this, _exceptionHandlerState);
+            }
+            return false;
+        }
+
+        /// <inheritdoc />
+        public void SetExceptionHandler(Func<Exception, IKcpTransport<KcpConversation>, object?, bool> handler, object? state)
+        {
+            _exceptionHandler = handler;
+            _exceptionHandlerState = state;
+        }
+
     }
 }
